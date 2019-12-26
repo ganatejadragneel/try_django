47e121b32d99b336663b197572493a86329deb29
Ganateja Akula <ganateja.akula1998@mail.com>
2019-12-27 00:56:36 +0530

    a new database lookup value

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -3,9 +3,8 @@ from django.http import Http404
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request,post_id):
-    print(post_id.__class__)
-    obj = get_object_or_404(BlogPost,id=post_id)
+def blog_post_detail_page(request,slug):
+    obj = get_object_or_404(BlogPost,slug=slug)
     template_name = "blog_post_detail.html"
     context= {"object": obj}
     return render(request,template_name,context)
