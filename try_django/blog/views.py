df0df33a21319759133d5346a6bd92f4729e9f7a
Ganateja Akula <ganateja.akula1998@mail.com>
2019-12-27 01:01:33 +0530

    queryset lookups

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -4,7 +4,10 @@ from django.http import Http404
 from .models import BlogPost
 
 def blog_post_detail_page(request,slug):
-    obj = get_object_or_404(BlogPost,slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count >= 1:
+        obj=queryset.first()
+    #obj = get_object_or_404(BlogPost,slug=slug)
     template_name = "blog_post_detail.html"
     context= {"object": obj}
     return render(request,template_name,context)
