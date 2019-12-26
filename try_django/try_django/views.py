fc23c5068b93df6347fa4823c300f47caeb22707
Ganateja Akula <ganateja.akula1998@mail.com>
2019-12-26 20:33:52 +0530

    rendering any kind of template

--- try_django/try_django/views.py
+++ try_django/try_django/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
     my_title="Hello there..."
-    return render(request, "hello_world.html",{"title":my_title})
+    context={"title":my_title}
+    template_name = "title.txt"
+    template_obj = get_template(template_name)
+    rendered_string = template_obj.render(context)
+    return render(request, "hello_world.html",{"title":rendered_string})
 
 def about_page(request):
     return render(request, "about.html",{"title":"About Us"})
 
 def contact_page(request):
     return render(request, "hello_world.html",{"title":"Contact Us"})
+    
+def example_page(request):
+    context={"title":"Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
