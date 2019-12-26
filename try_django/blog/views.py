d7bb75efa13650905eab376472a6541fc6de3715
Ganateja Akula <ganateja.akula1998@mail.com>
2019-12-26 23:23:58 +0530

    get object or 404

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 # Create your views here.
 from .models import BlogPost
 
 def blog_post_detail_page(request,post_id):
-    obj = BlogPost.objects.get(id=post_id)
+    print(post_id.__class__)
+    obj = get_object_or_404(BlogPost,id=post_id)
     template_name = "blog_post_detail.html"
     context= {"object": obj}
     return render(request,template_name,context)
