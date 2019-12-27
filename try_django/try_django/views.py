0ab5efe3563399a1139cc64637937696034a32c6
Ganateja Akula <ganateja.akula1998@mail.com>
2019-12-28 00:24:10 +0530

    django form

--- try_django/try_django/views.py
+++ try_django/try_django/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
 def home_page(request):
     my_title="Hello there..."
     context={"title":my_title,"my_list":[1,2,3,4,5]}
@@ -11,8 +13,15 @@ def about_page(request):
     return render(request, "about.html",{"title":"About Us"})
 
 def contact_page(request):
-    print(request.POST)
-    return render(request, "form.html",{"title":"Contact Us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        "title":"Contact Us",
+        "form":form
+    }
+    return render(request, "form.html",context)
     
 def example_page(request):
     context={"title":"Example"}
